9e080caef1bf978cd8fa2d921540d4c336b15d3c
Vladi11290 <153734058+Vladi11290@users.noreply.github.com>
2025-08-28 20:35:34 +0200

    commit 2 - Implementada barra de navegación y sección hero con carrusel de imágenes. Corregida la carga de imágenes en angular.json.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,33 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [NavbarComponent, NgFor, NgIf],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('mi-proyecto-angular');
+
+  images = [
+    'assets/img2.jpg',
+    'assets/imagen.jpg',
+    'assets/img3.jpg'
+  ];
+
+  currentSlide = signal(0);
+
+  nextSlide() {
+    this.currentSlide.update(i => (i + 1) % this.images.length);
+  }
+
+  prevSlide() {
+    this.currentSlide.update(i => (i - 1 + this.images.length) % this.images.length);
+  }
+
+  goToSlide(i: number) {
+    this.currentSlide.set(i);
+  }
 }
